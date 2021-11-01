1abd90b8bdb7dca96a4f47f6eb97120df3fe34f6
fortymiles <mqgao@outlook.com>
2021-11-01 20:32:53 +0800

    add type matches

--- python-deep-practice/python_pattern_match.py
+++ python-deep-practice/python_pattern_match.py
@@ -4,9 +4,22 @@ def match_feature(x):
             print(a)
         case a, b, c:
             print(c)
+        case {'name': name, 'age': age, 'register': {'new': _new}}:
+            print(name, age, _new)
         case _:
             print('not fit')
 
 
+def match_types(input_content):
+    match input_content:
+        case float() | int():
+            return input_content, 'is number'
+        case str():
+            return input_content, 'is string'
+        case _:
+            return 'not anything'
+
+
 if __name__ == '__main__':
-    match_feature((1, 2))
+    print(match_types('hello'))
+    print(match_types(10))
